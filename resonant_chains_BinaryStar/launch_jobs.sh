07658dd99618c12898c2f0659f0a9eecb6855d25
ErwanH29 <e.j.s.g.hochart@umail.leidenuniv.nl>
2025-12-30 17:01:21 +0100

    Cleaned repo

--- resonant_chains_BinaryStar/launch_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {0..75}
-do
-   start=$((i * 2))
-   end=$(((i+1) * 2))
-   echo "Slice $star:$end"
-   nohup python resonance_chain_planetary_system.py $start $end >text/out${i}.txt &
-done
